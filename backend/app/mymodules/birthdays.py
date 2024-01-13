cd5be511d7df74ba87c891ba4239547890ab9bb5
marcopellizzon <150334423+marcopellizzon@users.noreply.github.com>
2024-01-13 16:19:05 +0100

    Update birthdays.py with the first function

--- backend/app/mymodules/birthdays.py
+++ backend/app/mymodules/birthdays.py
@@ -1,21 +1,33 @@
-birthdays = {
-    'Albert Einstein': '03/14/1879',
-    'Benjamin Franklin': '01/17/1706',
-    'Ada Lovelace': '12/10/1815',
-    'Donald Trump': '06/14/1946',
-    'Rowan Atkinson': '01/6/1955'}
+import pandas as pd
 
-# Extract from birthdays dictionary the names of the people and add them in a string
-def print_birthdays_str():
-    print('Welcome to the birthday dictionary. We know the birthdays of these people:')
-    names = ''
-    for name in birthdays:
-        names += name + ', '
-    return names[:-2]
+data = pd.read_csv('/app/app/filedati.csv', delimiter=';')
 
+# Function1 - total_waste
+# This function takes three parameters:
+# the name of a "Comune", a year, and the path to the CSV file.
+# It searches the CSV file for the given "Comune" and year and
+# returns the corresponding “Rifiuto totale (in Kg)” value.
 
-def return_birthday(name):
-    if name in birthdays:
-        return('{}\'s birthday is {}.'.format(name, birthdays[name]))
+def total_waste(comune, year, data):
+    """
+    Function to retrieve the total waste for a given comune and year.
+
+    :param comune: Name of the Comune
+    :param year: Year of interest
+    :param file_path: Path to the CSV file
+    :return: Total waste in Kg or a message if not found
+    """
+    
+    # Load the CSV file with the correct delimiter
+    data = pd.read_csv('/app/app/filedati.csv', delimiter=';')
+
+    # Filter data for the given comune and year
+    filtered_data = data[(data['Comune'] == comune) & (data['Anno'] == year)]
+
+    # Check if there is an entry for the given comune and year
+    if not filtered_data.empty:
+        # Extract the total waste value
+        total_waste = filtered_data['Rifiuto totale (in Kg)'].iloc[0]
+        return total_waste
     else:
-        return('Sadly, we don\'t have {}\'s birthday.'.format(name))
+        return "No data found for the specified Comune and Year."
